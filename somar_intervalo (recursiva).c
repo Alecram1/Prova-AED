4d772bb01062e18e8d2b1f00b6a0d9d090bd4b68
Alecram1 <153995944+Alecram1@users.noreply.github.com>
2026-05-02 13:03:10 -0300

    Add files via upload

---
+++ somar_intervalo (recursiva).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int somar_intervalo(Lista *l,int a,int b){
+    if(l==NULL)
+        return 0;
+    if(l->info>=a && l->info<=b)
+        return l->info + somar_intervalo(l->prox,a,b);
+    return somar_intervalo(l->prox,a,b);
+}

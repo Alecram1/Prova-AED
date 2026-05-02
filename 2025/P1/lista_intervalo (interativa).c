0e80a34d595405680338fe33af6fcc2710b39c61
Alecram1 <153995944+Alecram1@users.noreply.github.com>
2026-05-02 19:18:00 -0300

    Create lista_intervalo (interativa).c

---
+++ 2025/P1/lista_intervalo (interativa).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "struct_no.h"
+
+Lista* lista_intervalo(Lista* l,int a,int b){
+	Lista* l2=NULL; //lista que ser� retornada
+	Lista* aux=l; //verifica cada elemento da lista
+	while(aux!=NULL){
+		if(aux->info>=a && aux->info<=b)//caso esteja dentro do intervalo
+			l2=inserir(l2,aux->info);
+		aux=aux->prox;
+	}
+	return inverter(l2); //retorna o intervalo
+}

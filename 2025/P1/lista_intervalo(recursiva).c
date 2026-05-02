534e423f7b108dda5da407b7fdf58e54bd34aa8a
Alecram1 <153995944+Alecram1@users.noreply.github.com>
2026-05-02 20:00:33 -0300

    Update lista_intervalo(recursiva).c

--- 2025/P1/lista_intervalo(recursiva).c
+++ 2025/P1/lista_intervalo(recursiva).c
@@ -2,12 +2,13 @@
 #include "struct_no.h"
 
 Lista* lista_intervalo(Lista* l,int a,int b){
-    Lista* nova=null;
+    Lista* nova=NULL;
     if(l==NULL)//caso chegue ao final da lista
         return nova;
     if(l->info>=a && l->info<=b){//caso esteja dentro do intervalor
-        nova=inserir(nova,l->info);
-        nova->prox=lista_intervalo(l->prox,a,b);//concatenando os n�s
+        Lista* nova=(Lista*)malloc(seizeof(Lista));// OBS: Nao esta sendo usada a funcao "inserir" pois ela coloca o proximo no como NULL
+        nova->info=l->info;
+        nova->prox=lista_intervalo(l->prox,a,b);//encadeia com o proximo elemento da lista
         return nova;//retorna a lista com o elemento inserido
     }
     return lista_intervalo(l->prox,a,b);

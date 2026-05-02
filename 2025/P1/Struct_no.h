529a0e6061a3918d6eb618f326da404f3e6c074e
Alecram1 <153995944+Alecram1@users.noreply.github.com>
2026-05-02 19:16:39 -0300

    Create Struct_no.h

---
+++ 2025/P1/Struct_no.h
@@ -0,0 +1,27 @@
+#ifndef LISTA.H
+#define LISTA.H
+#include <stdlib.h>
+
+typedef struct no{
+    int info;
+    struct no *prox;
+}Lista;
+
+Lista* inserir(Lista* l,int info){
+    Lista* aux=(Lista*)malloc(sizeof(Lista)); //Reserva um espa�o na memoria para o novo elemento
+    aux->info=info;
+    aux->prox=l;
+    return aux;//retorna uma lista ja com o novo elemento
+}
+
+Lista* inverter(Lista* l){
+    Lista* aux=l;//usada para percorrer a lista
+    Lista* nova=NULL;//guarda a inversao
+    while(aux!=NULL){
+        nova=inserir(nova,aux->info);
+        aux=aux->prox;
+    }
+    return nova;//retorna a lista invertida
+}
+
+#endif

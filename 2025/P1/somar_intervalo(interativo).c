c8e331e0c2191f468806562ce5db8ab1b5a6499b
Alecram1 <153995944+Alecram1@users.noreply.github.com>
2026-05-02 19:21:51 -0300

    Update somar_intervalo(interativo).c

--- 2025/P1/somar_intervalo(interativo).c
+++ 2025/P1/somar_intervalo(interativo).c
@@ -2,12 +2,12 @@
 #include "struct_no.h"
 
 int soma_intervalo(Lista* l,int a,int b){
-    Lista* aux=l;
+    Lista* aux=l; //percore a lista
     int somador=0;
     while(aux!=NULL){
-        if(aux->info>=a && aux->info<=b)
+        if(aux->info>=a && aux->info<=b) //caso encontre algum elemento
             somador+=aux->info
-        aux=aux->prox;
+        aux=aux->prox; //continua percorrendo a lista
     }
-    return somador;
+    return somador; //retorna a soma total
 }

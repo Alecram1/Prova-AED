10206851469c37927f793354d5a7e40506dba263
Alecram1 <153995944+Alecram1@users.noreply.github.com>
2026-05-02 19:20:42 -0300

    Update somar_intervalo (recursiva).c

--- 2025/P1/somar_intervalo (recursiva).c
+++ 2025/P1/somar_intervalo (recursiva).c
@@ -2,8 +2,8 @@
 #include "struct_no.h"
 int somar_intervalo(Lista *l,int a,int b){
     if(l==NULL)
-        return 0;
-    if(l->info>=a && l->info<=b)
-        return l->info + somar_intervalo(l->prox,a,b);
-    return somar_intervalo(l->prox,a,b);
+        return 0;//caso chegue ao final da lista
+    if(l->info>=a && l->info<=b)//se o elementos estiver dentro do intervalo
+        return l->info + somar_intervalo(l->prox,a,b);//retorna a soma de todos os elementos
+    return somar_intervalo(l->prox,a,b);//vai para o proximo elemento da lista
 }
